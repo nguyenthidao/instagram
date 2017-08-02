fdce86f004a8628c2d1eea8c52fcf2216f54d623
Đào <dao@nals-Mac-mini.local>
2017-08-02 17:38:17 +0700

    create a user not yet jwt

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,15 +6,6 @@ var express = require('express'),
 	methodOverride = require('method-override')
 	;
 
-router.use(bodyParser.urlencoded())
-router.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}));
-
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/images')
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,10 +6,76 @@ var express = require('express'),
 	methodOverride = require('method-override')
 	;
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+var upload = multer({ 
+	storage: storage 
+});
+
+router.get('/', function(req, res, next){
+		mongoose.model('User').find({}, function (err, user) {
+			if(err){
+				res.json({
+					status: 500,
+					err: "create user error"
+				});
+			}else{
+				res.json({
+					user : user
+				});
+			}
+		});
+});
+
+router.post('/', upload.single('feature'), function(req, res) {
+	var username = req.body.username;
+	var fullname = req.body.fullname;
+	var password = req.body.password;
+	var short_description = req.body.short_description;
+	var feature = '/images/' + req.file.filename;
+	var followers = [];
+	var following = [];
+	var requested_by = [];
+	var posts = [];
+
+	mongoose.model('User').create({
+		username: username,
+		fullname: fullname,
+		password: password,
+		short_description: short_description,
+		avatar : feature,
+		followers: followers,
+		following: following,
+		requested_by: requested_by,
+		posts: posts
+	}, function (err, user) {
+		if(err){
+			res.json({
+				status: 500,
+				err: "create user error"
+			});
+		}else{
+			res.json({
+				user: user
+			});
+		}
+	});
+});
+
 router.get('/:id', function(req, res){
 	mongoose.model('User').findById(req.params.id, function(err, user){
 		if(err){
-			res.send(err);
+			res.json({
+				status: 500,
+				err: "create user error"
+			});
 		}else{
 			res.json({
 				data: {
@@ -30,14 +96,20 @@ router.get('/:id', function(req, res){
 router.get('/:id/follows', function(req, res){
 	mongoose.model('User').findById(req.params.id, function(err, users){
 		if(err){
-			res.send(err);
+			res.json({
+				status: 500,
+				err: "create user error"
+			});
 		}else{
 			var follows = [];
 			var follows_id = users.following;
 
 			mongoose.model('User').find({_id: {$in: follows_id}}, function(err, follows){
 				if(err){
-					res.send(err);
+					res.json({
+						status: 500,
+						err: "create user error"
+					});
 				}else{
 					res.json({
 						follows: follows
@@ -51,14 +123,20 @@ router.get('/:id/follows', function(req, res){
 router.get('/:id/followed-by', function(req, res){
 	mongoose.model('User').findById(req.params.id, function(err, users){
 		if(err){
-			res.send(err);
+			res.json({
+				status: 500,
+				err: "create user error"
+			});
 		}else{
 			var followers = [];
 			var followers_id = users.followers;
 
 			mongoose.model('User').find({_id: {$in: followers_id}}, function(err, followers){
 				if(err){
-					res.send(err);
+					res.json({
+						status: 500,
+						err: "create user error"
+					});
 				}else{
 					res.json({
 						followers: followers
@@ -72,14 +150,20 @@ router.get('/:id/followed-by', function(req, res){
 router.get('/:id/requested-by', function(req, res){
 	mongoose.model('User').findById(req.params.id, function(err, users){
 		if(err){
-			res.send(err);
+			res.json({
+				status: 500,
+				err: "create user error"
+			});
 		}else{
 			var requested_by = [];
 			var requested_by_id = users.requested_by;
 
 			mongoose.model('User').find({_id: {$in: requested_by_id}}, function(err, requested_by){
 				if(err){
-					res.send(err);
+					res.json({
+						status: 500,
+						err: "create user error"
+					});
 				}else{
 					res.json({
 						requested_by: requested_by
